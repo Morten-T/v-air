cc70d91779903ecb46a7f1bbf9714a16eb99e397
MortenT <mtlnielsen@gmail.com>
2026-02-09 13:43:55 +0100

    resized for small screen

--- src/components/VairDay.tsx
+++ src/components/VairDay.tsx
@@ -8,18 +8,18 @@ function VairDay({ vair }: VairDayProps) {
   const downPour: boolean = vair.rain > 0 || vair.snow > 0;
   const stableTemp: boolean = vair.max_temperature === vair.min_temperature;
   return (
-    <div className="flex flex-col p-4 sm:p-6 gap-3 box-border border-2 bg-slate-600 rounded-lg w-full sm:w-72 h-auto sm:h-80 min-w-max sm:min-w-fit">
+    <div className="flex flex-col p-4 gap-2 box-border border-2 bg-slate-600 rounded-lg w-52 h-60 min-w-fit">
       {/* <div className="text-black box-border border-4 bg-slate-50">❤</div> */}
-      <h2 className="text-5xl">{vair.time}</h2>
-      <div className="flex flex-col gap-2">
-        <p className="text-4xl">
+      <h2 className="text-4xl">{vair.time}</h2>
+      <div className="flex flex-col gap-1">
+        <p className="text-3xl">
           {stableTemp
             ? vair.max_temperature + "°"
             : `${vair.min_temperature}° / ${vair.max_temperature}°`}
         </p>
-        <p className="text-xl">Vindhastighed: {vair.windSpeed} km/t</p>
+        <p className="text-lg">Vindhastighed: {vair.windSpeed} km/t</p>
         {downPour && (
-          <p className="text-xl">
+          <p className="text-lg">
             Nedbør:
             {vair.rain > vair.snow
               ? " " + vair.rain + " mm regn 🌧️"
