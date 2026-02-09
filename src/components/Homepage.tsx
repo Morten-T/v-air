d128eee6734b831f68d1b310c6a6da6532e5b47a
Morten-T <AU721758@uni.au.dk>
2026-02-09 15:41:43 +0100

    Loading og error States

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -9,28 +9,32 @@ import byskilt from "../assets/AarhusByskilt.png";
 // starter ud med homepage for at have et skelet jeg kan proppe noget data i
 function Homepage() {
   const dummyCurrentVair: CurrentVair = {
-    temperature: 5,
-    apparent_temperature: 3,
-    humidity: 15,
-    snow: 3,
+    temperature: 0,
+    apparent_temperature: 0,
+    humidity: 0,
+    snow: 0,
     rain: 0,
-    windSpeed: 5,
+    windSpeed: -1,
   };
   const dummyVair: Vair = {
-    time: "Tirsdag",
-    max_temperature: 5,
-    min_temperature: -3,
-    snow: 4,
+    time: "Error",
+    max_temperature: 0,
+    min_temperature: 0,
+    snow: 0,
     rain: 0,
-    windSpeed: 10,
+    windSpeed: -1,
   };
 
   const [forecast, setForecast] = useState<Vair[]>([]);
   const [currentVair, setCurrentVair] = useState<CurrentVair>(dummyCurrentVair);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setIsLoading(true);
+        setError(null);
         const fetchedCurrentVair = await fetchCurrentWeather();
         setCurrentVair(fetchedCurrentVair);
 
@@ -38,6 +42,10 @@ function Homepage() {
         setForecast(fetchedDailyVair);
       } catch (error) {
         console.error("Error fetching weather: ", error);
+        setError("Fejl i WeatherData fetch, tjek konsollen");
+        // setForecast(Array(7).fill(dummyVair));
+      } finally {
+        setIsLoading(false);
       }
     };
     fetchData();
@@ -52,13 +60,18 @@ function Homepage() {
 
   return (
     <div className="homepage flex flex-col pl-4 pt-8 w-screen h-screen gap-[3vh]">
+      {isLoading && (
+        <p className="text-[4vh] text-slate-200">Henter vejrdata...</p>
+      )}
+      {error && <p className="text-[4vh] text-red-300">{error}</p>}
       {/* Vejret i dag */}
       <div className="flex flex-row items-center">
-        <div className="flex flex-row gap-4">
-          <CurrentDay currentVair={currentVair} />
-          <p className="text-[6vh]">{downpurType}</p>
-        </div>
-
+        {!isLoading && !error && (
+          <div className="flex flex-row gap-4">
+            <CurrentDay currentVair={currentVair} />
+            <p className="text-[6vh]">{downpurType}</p>
+          </div>
+        )}
         <div className="flex-1 flex justify-center">
           <img
             src={byskilt}
@@ -69,12 +82,25 @@ function Homepage() {
         </div>
       </div>
       {/* Uge prognose */}
-      <h2 className="text-[6vh] pl-8">Kommende uge</h2>
-      <div className="week pt-2 px-4 gap-4 flex flex-row flex-wrap lg:flex-nowrap overflow-hidden">
-        {forecast.slice(1).map((vair, index) => (
-          <VairDay key={index} vair={vair} />
-        ))}
-      </div>
+      {!isLoading && !error && (
+        <>
+          <h2 className="text-[6vh] pl-8">Kommende uge</h2>
+          <div className="week pt-2 px-4 gap-4 flex flex-row flex-wrap lg:flex-nowrap overflow-hidden">
+            {forecast.slice(1).map((vair, index) => (
+              <VairDay key={index} vair={vair} />
+            ))}
+          </div>
+        </>
+      )}
+
+      {/* Error block */}
+      {error && !isLoading && (
+        <div className="flex flex-col items-center justify-center flex-1">
+          <p className="text-[5vh] text-red-400">
+            Kunne ikke hente uge prognose
+          </p>
+        </div>
+      )}
     </div>
   );
 }
